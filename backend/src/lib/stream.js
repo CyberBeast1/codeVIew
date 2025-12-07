1e575d52f0b7bbaabb69c3852edf24b4392eba79
CyberBeast1 <cyberdart1410@gmail.com>
2025-12-08 03:54:05 +0530

    added auth and stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,29 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  throw new Error("Stream API key and secret must be provided");
+}
+
+export const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUser(userData);
+    console.log("Stream user upserted successfully:", userData);
+  } catch (error) {
+    error("Error upserting Stream user:", error);
+  }
+};
+
+export const deleteStreamUser = async (userId) => {
+  try {
+    await streamClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userData);
+  } catch (error) {
+    error("Error deleting Stream user:", error);
+  }
+};
