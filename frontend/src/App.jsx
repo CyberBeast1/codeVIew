ad1a3bdb1b1c172a93c35546fdaf570961570db2
CyberBeast1 <cyberdart1410@gmail.com>
2025-12-08 18:46:44 +0530

    implemented landing page and problems page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,32 @@ import { Navigate, Route, Routes } from "react-router";
 import { useUser } from "@clerk/clerk-react";
 import HomePage from "./pages/HomePage.jsx";
 import ProblemsPage from "./pages/ProblemsPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
 
+  if (!isLoaded) return null;
+  
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/problems"
-        element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
-      />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={isSignedIn ? <Navigate to={"/dashboard"} /> : <HomePage />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
 
-    <Toaster position="bottom-right" toastOptions={{duration: 2000}}/>
+      <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
     </>
   );
 }
